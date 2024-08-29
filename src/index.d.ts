4c788a297462c43c09a2295c558bdf8f5ae09267
Gugle <administration@ztxy666.cn>
2024-08-29 21:21:18 +0800

    update

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class EventManager {
-    private readonly parent;
-    private readonly eventListeners;
-    constructor(parent?: EventManager | null);
-    listen(event: string, callback: (...args: any) => void, namespace?: string, priority?: number, cancelable?: boolean): void;
-    subscribe(event: string, namespace?: string, priority?: number, cancelable?: boolean): (callback: (...args: any) => void) => void;
-    post(event: string, ...args: any): Promise<any>;
-    remove(namespace: string): void;
-}
